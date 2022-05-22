445cdf13400fb1cde0ff76c30e4d394d0c2491bf
Vitaly Bogryakov <vitjrbog@gmail.com>
2022-05-22 17:13:09 +0500

    'add' API method has been described for Notes app

--- handbook/apps/notes/views.py
+++ handbook/apps/notes/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import datetime
+import time
+import hashlib
 
-# Create your views here.
+from django.http import HttpRequest, JsonResponse
+from . import models
+
+
+def add(request: HttpRequest) -> JsonResponse:
+    if request.method != 'POST':
+        return JsonResponse({
+            'status_code': 400,
+            'error': f'{request.method} is not allowed.',
+        })
+
+    if request.POST.get('text') is None:
+        return JsonResponse({
+            'status_code': 400,
+            'error': 'attribute "text" is required',
+        })
+    text = str(request.POST.get('text'))
+    title = __compose_title(text)
+    date = __get_date()
+
+    note = models.Note()
+    note.create(title, date)
+
+    note_id = models.Note.objects.last().id
+
+    checksum = __gen_checksum(text)
+
+    version = models.Version()
+    version.create(text, date, checksum, note_id)
+
+    return JsonResponse({
+        'status_code': 200
+    })
+
+def __compose_title(text: str) -> str:
+    if len(text) > 47:
+        return f'{text[:46]}...'
+    return text
+
+def __get_date() -> int:
+    today = datetime.datetime.now()
+    
+    return int(time.mktime(today.timetuple()))
+
+def __gen_checksum(text: str) -> str:
+    hash = hashlib.md5(text.encode('utf-8'))
+    return hash.hexdigest()
