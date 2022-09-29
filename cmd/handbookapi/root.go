5fffb69399db91f84a16147be26407ca146ae7df
Vitaly Bogryakov <vitjrbog@gmail.com>
2022-09-29 09:54:15 +0500

    Described an algorithm of application starting up

--- cmd/handbookapi/root.go
+++ cmd/handbookapi/root.go
@@ -1,7 +1,24 @@
 package handbookapi
 
-import "fmt"
+import (
+	"fmt"
+	"handbook-api/internal/config"
+	"handbook-api/internal/db"
+	"handbook-api/internal/server"
+)
 
+// Execute starts the main functions of program.
 func Execute() {
-	fmt.Println("Hello world")
+	dbConnCfg := config.NewDBConnectionConfig()
+
+	dsn := fmt.Sprintf("%s://%s:%s@%s:%s/%s",
+		dbConnCfg.DBMS(),
+		dbConnCfg.DBMSUserName(), dbConnCfg.DBMSUserPassword(),
+		dbConnCfg.Host(), dbConnCfg.Port(), dbConnCfg.DBName())
+
+	dbConn := db.NewConnection(dsn)
+
+	serverCfg := config.NewServerConfig()
+
+	server.Up(dbConn, serverCfg)
 }
