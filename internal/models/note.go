ec32840239682141ea8a64d652e5e510f3896156
Vitaly Bogryakov <vitjrbog@gmail.com>
2022-10-30 10:41:54 +0500

    Changed type of field 'Date' of 'Note' model

--- internal/models/note.go
+++ internal/models/note.go
@@ -1,10 +1,8 @@
 package models
 
-import "time"
-
 // Note store a data from 'notes' table.
 type Note struct {
-	ID    int       `json:"id"`
-	Title string    `json:"title"`
-	Date  time.Time `json:"date"`
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Date  int64  `json:"date"`
 }
