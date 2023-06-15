5c53e0c02bc6c9aeced01a7d70485dd9395a14df
Vitaly Bogryakov <vitjrbog@gmail.com>
2023-06-15 17:08:00 +0500

    New constraints for the database tables
    The 'NOT NULL' constraints have been added to the 'c_date', 'checksum' and 'note_id' fields in the 'note' and 'version' tables.

--- docker/psql/migrate.sql
+++ docker/psql/migrate.sql
@@ -4,16 +4,16 @@ DROP TABLE IF EXISTS note;
 
 CREATE TABLE note (
     id SERIAL NOT NULL UNIQUE,
-    c_date TEXT,
+    c_date TEXT NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE version (
     id SERIAL NOT NULL UNIQUE,
     full_text TEXT,
-    c_date TEXT,
-    checksum TEXT,
-    note_id INTEGER,
+    c_date TEXT NOT NULL,
+    checksum TEXT NOT NULL,
+    note_id INTEGER NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT fk_note FOREIGN KEY(note_id) REFERENCES note(id)
 );
